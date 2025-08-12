29c84b6eeb7f103a7c99a001c5b0e8882ba03a53
sobirooov <hackher_boy@list.ru>
2025-08-12 17:08:53 +0500

    IpBlackListFilter Added

---
+++ src/main/java/uz/azamjon/filter/IpBlackListFilter.java
@@ -0,0 +1,31 @@
+package uz.azamjon.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IpBlackListFilter implements Filter {
+    public void  init(FilterConfig filterConfig) throws ServletException {
+
+    }
+    private List<String> ipBlackList;
+
+    {
+        ipBlackList = new ArrayList<>();
+        ipBlackList.add("192.168.100.120");
+    }
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String ipAddress = request.getRemoteAddr();
+        System.out.println(ipAddress);
+        if (ipBlackList.contains(ipAddress)) {
+            chain.doFilter(request, response);
+        } else  {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+    }
+    public void destroy() {}
+
+}
